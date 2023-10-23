f380789ce287100e0541398a1c62122bb939bc35
Naman Aggarwal <naman.aggarwal02@ad.infosys.com>
2023-10-23 21:02:04 +0530

    Changed git folder

---
+++ src/Demo.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './Demo.css'
+
+// class Demo extends Component{
+//     render() {
+//         return <div className='divStyle'>
+//             <h1>
+//                 Hello World!!
+//             </h1>
+//             <p>
+//                 This is my First React App
+//             </p>
+//         </div>
+//     }
+// }
+
+const Demo = () => {
+    return <div>
+        <h1>
+            Hola Amigos!
+        </h1>
+        <p>
+            This is a React App using functional component
+        </p>
+    </div>
+}
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Demo from './Demo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Demo />
   </React.StrictMode>
+
+  // <h1>Hello World</h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
